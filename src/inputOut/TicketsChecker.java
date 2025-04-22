1da53a2e10054f6a9441a8b6a418d1ccce85d2ba
GostWave <gostwave@mail.ru>
2025-04-22 16:53:30 +0300

    Upgrade

--- src/inputOut/TicketsChecker.java
+++ src/inputOut/TicketsChecker.java
@@ -18,14 +18,23 @@ public class TicketsChecker {
     }
     public static boolean checkTicket(Ticket ticket){
         Ticket ticket_new = new Ticket();
-        return TicketFiller.fillName(ticket_new, ticket.getName()) &
-                TicketFiller.fillType(ticket_new, ticket.getType().name()) &
-                TicketFiller.fillRefundable(ticket_new, String.valueOf(ticket.getRefundable())) &
-                TicketFiller.fillDiscount(ticket_new, String.valueOf(ticket.getDiscount())) &
-                TicketFiller.fillPrice(ticket_new, String.valueOf(ticket.getPrice())) &
-                TicketFiller.fillCoordinates(ticket_new, new Coordinates(), String.valueOf(ticket.getCoordinates().getX())+ " " + String.valueOf(ticket.getCoordinates().getY())) &
-                TicketFiller.fillEvent(new Event(), ticket.getEvent().toStringForCheck().split(" ")[0], ticket.getEvent().toStringForCheck().split(" ")[1], ticket.getEvent().toStringForCheck().split(" ")[2], ticket.getEvent().toStringForCheck().split(" ")[3]);
+        if (ticket.getEvent()==null){
+            return TicketFiller.fillName(ticket_new, ticket.getName()) &
+                    TicketFiller.fillType(ticket_new, ticket.getType().name()) &
+                    TicketFiller.fillRefundable(ticket_new, String.valueOf(ticket.getRefundable())) &
+                    TicketFiller.fillDiscount(ticket_new, String.valueOf(ticket.getDiscount())) &
+                    TicketFiller.fillPrice(ticket_new, String.valueOf(ticket.getPrice())) &
+                    TicketFiller.fillCoordinates(ticket_new, new Coordinates(), String.valueOf(ticket.getCoordinates().getX())+ " " + String.valueOf(ticket.getCoordinates().getY())) ;
 
+        } else {
+            return TicketFiller.fillName(ticket_new, ticket.getName()) &
+                    TicketFiller.fillType(ticket_new, ticket.getType().name()) &
+                    TicketFiller.fillRefundable(ticket_new, String.valueOf(ticket.getRefundable())) &
+                    TicketFiller.fillDiscount(ticket_new, String.valueOf(ticket.getDiscount())) &
+                    TicketFiller.fillPrice(ticket_new, String.valueOf(ticket.getPrice())) &
+                    TicketFiller.fillCoordinates(ticket_new, new Coordinates(), String.valueOf(ticket.getCoordinates().getX()) + " " + String.valueOf(ticket.getCoordinates().getY())) &
+                    TicketFiller.fillEvent(new Event(), ticket.getEvent().toStringForCheck().split(" ")[0], ticket.getEvent().toStringForCheck().split(" ")[1], ticket.getEvent().toStringForCheck().split(" ")[2], ticket.getEvent().toStringForCheck().split(" ")[3]);
+        }
 
 
     }

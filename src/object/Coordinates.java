176a7066d5ee927ba107b2f129f7beb527d4c861
Ellixis <beloshitskiyf@mail.ru>
2025-04-22 10:02:31 +0300

    sg

--- src/object/Coordinates.java
+++ src/object/Coordinates.java
@@ -3,4 +3,20 @@ package object;
 public class Coordinates {
     private int x;
     private double y; //Значение поля должно быть больше -174
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
 }
